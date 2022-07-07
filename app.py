82d2066587570f26af37938f47fc9dd182c45f70
Henry Nguyen <henrytbnguyen11@yahoo.com>
2022-07-06 22:50:06 -0700

    Adjusted passing of JSON data

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     population = resp.read().decode()
     population = json.loads(population)
 
-    return render_template('index.html', total_pop = population["totalPopulation"])
+    return render_template('index.html', total_pop = population)
 
 if __name__ == '__main__':
     app.run(debug=True)
