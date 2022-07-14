8ec70a35af18e872eb9a0afbf89ed95accbf2f76
Henry Nguyen <henrytbnguyen11@yahoo.com>
2022-07-13 20:17:27 -0700

    Created display for population by district

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ def index():
     resp = request.urlopen(popLink)
     population = resp.read().decode()
     population = json.loads(population)
+    sortedDistricts = {dis: pop for dis, pop in sorted(population["populationByDistrict"].items())}
 
-    return render_template('index.html', total_pop = population)
+    return render_template('index.html', total_pop = population, popByDistrict = sortedDistricts)
 
 if __name__ == '__main__':
     app.run(debug=True)
